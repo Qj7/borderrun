d78619c94d757b2f5110c756a03d1f2397ca96b6
Vladimir Dolgiy <dolgiy.v.v@gmail.com>
2024-01-17 12:09:56 +0700

    fx

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Borderrun
     config.assets.js_compressor = :terser
     config.assets.css_compressor = :sass
 
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :ru]
 
     config.active_record.async_query_executor = :global_thread_pool
