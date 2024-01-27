f9d0a42f3c757e628e80b13858a1b19fd09aed65
Vladimir Dolgiy <dolgiy.v.v@gmail.com>
2024-01-27 12:35:40 +0700

    fx

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( jquery.min.js jquery_ujs.js bootstrap.min.js popper.js *.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf *.webp *.sccs *.css *.mp4)
+Rails.application.config.assets.precompile += %w( application.js )
