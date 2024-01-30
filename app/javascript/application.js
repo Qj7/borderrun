c0b9b8ca3cedaf6b40d593439dd5bbdabdd24ae4
Vladimir Dolgiy <dolgiy.v.v@gmail.com>
2024-01-30 10:20:24 +0700

    fx

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,18 +8,23 @@ import 'popper';
 import 'bootstrap';
 import 'datepicker';
 
-window.onload = function() {
-  var loadWrapper = document.getElementById('loadWrapper');
-  loadWrapper.style.display = 'none';
-};
+// window.onload = function() {
+//   var loadWrapper = document.getElementById('loadWrapper');
+//   loadWrapper.style.display = 'none';
+// };
 
-// document.addEventListener('turbo:before-render', function() {
-//   loadWrapper.style.display = 'block';
-// });
+document.addEventListener('turbo:before-render', function() {
+  loadWrapper.style.display = 'block';
+});
 
 // document.addEventListener('turbo:load', function() {
 //   loadWrapper.style.display = 'none';
 // });
+
+document.addEventListener('turbo:render', function() {
+  loadWrapper.style.display = 'none';
+});
+
 
 document.getElementsByTagName('source').addEventListener('ended', function() {
   this.currentTime = 1;
